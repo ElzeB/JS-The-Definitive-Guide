1814f31ea0eca1f63aba2a2a9115940c3b50975b
ElzeB <bernackyte@gmail.comls>
2020-09-11 13:58:52 +0300

    Type Conversions

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// let x=2, y=3;
+// console.log(Number("3"));
+
+// console.log(String(false));
+
+let n = 17;
+let binary = "Ob" + n.toString(2);
+let octal = "Oo" + n.toString(8);
+let hex = "Ox"+ n.toString(16);
+
+let b = 123456.789;
+
+console.log(parseFloat("3.14 meters"));
