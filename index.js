437e6e3293aefa1f1a1328ac91975439e1275ac2
ElzeB <bernackyte@gmail.comls>
2020-09-12 21:50:33 +0300

    Finihing Types and Values

--- index.js
+++ index.js
@@ -8,9 +8,25 @@ let n = 17;
 let binary = "Ob" + n.toString(2);
 let octal = "Oo" + n.toString(8);
 let hex = "Ox"+ n.toString(16);
+let d = new Date(2020, 2, 15);
+let e = new Date();
 
 let b = 123456.789;
 
-console.log(parseFloat("3.14 meters"));
 
-// comments added
+
+// console.log(parseFloat("3.14 meters"));
+// console.log([1,2,3].toString());
+// console.log(d.toString());
+// console.log(e.toString());
+
+// console.log(n.valueOf());
+console.log(d.valueOf());
+
+let i;
+let sum;
+let i, sum;
+
+
+
+
