85aa093bfb06be11fb5d1ea85e2493eaa3fe9194
ElzeB <bernackyte@gmail.comls>
2020-09-14 13:57:39 +0300

    Expressions and Operators first commit

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-//Testing second branch creation;
+let o = {
+    x: 1,
+    y: {
+        z: 3
+    }
+};
+
+let a = [o, 4, [5, 6]];
+
+// console.log(o.x);
+// console.log(o.y.z);
+// console.log(o["x"]);
+console.log(a[1]);
+console.log(a[2]["1"]);
+
+
+
+
 
 
 
